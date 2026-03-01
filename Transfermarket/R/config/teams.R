852290cf68023279a35e4bac5a6edcbd57070828
yellowhorsestudios-create <yellowhorsestudios@gmail.com>
2026-03-01 22:01:30 +0100

    Add files via upload

---
+++ Transfermarket/R/config/teams.R
@@ -0,0 +1,12 @@
+library(tibble)
+
+teams <- tribble(
+  ~team_name, ~team_slug, ~team_id,
+  "Arsenal", "arsenal-fc", 11,
+  "Manchester City", "manchester-city", 281,
+  "Aston Villa", "aston-villa", 405,
+  "Chelsea", "chelsea-fc", 631,
+  "Manchester United", "manchester-united", 985,
+  "Liverpool", "liverpool-fc", 31,
+  "Tottenham", "tottenham-hotspur", 148
+)
